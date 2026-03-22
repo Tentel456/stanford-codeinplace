dd55d58af4187ecc736f89385ddf23a1904e845b
Tentel456 <126483734+Tentel456@users.noreply.github.com>
2026-03-22 16:24:41 +0300

    Create main.py

---
+++ task-1/main.py
@@ -0,0 +1,15 @@
+from karel.stanfordkarel import *
+
+def main():
+    
+    """
+    Places 10 beepers in the spot that Karel is standing.
+    """
+    for i in range(10):
+        put_beeper()
+
+
+
+
+if __name__ == '__main__':
+    main()
